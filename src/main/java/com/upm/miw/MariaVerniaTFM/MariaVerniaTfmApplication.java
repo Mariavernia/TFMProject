77a3f3fe26c4a50b512bb94fa420fb7deea80957
Mariavernia <maversa2@alumni.uv.es>
2022-02-20 12:12:22 +0100

    Create spring project

---
+++ src/main/java/com/upm/miw/MariaVerniaTFM/MariaVerniaTfmApplication.java
@@ -0,0 +1,13 @@
+package com.upm.miw.MariaVerniaTFM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MariaVerniaTfmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MariaVerniaTfmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upm/miw/MariaVerniaTFM/MariaVerniaTfmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upm.miw.MariaVerniaTFM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariaVerniaTfmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
